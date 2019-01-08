d17d2b39a1d3e2e7a72bb2b37c844e714237b171
FleetCooling <kevlingo@gmail.com>
2019-01-08 14:52:12 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,12 @@ app.post('/todos', (req, res) => {
   console.log(req.body);
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then(todos => res.send({ todos }))
+    .catch(e => res.status(400).send(e));
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
