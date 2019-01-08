5f409f80a4557cefc1bce9b978a323facb9cf90d
FleetCooling <kevlingo@gmail.com>
2019-01-08 17:37:55 -0500

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let { ObjectID } = require('mongodb');
 
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
@@ -24,6 +25,23 @@ app.get('/todos', (req, res) => {
   Todo.find()
     .then(todos => res.send({ todos }))
     .catch(e => res.status(400).send(e));
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  User.findById(id)
+    .then(user => {
+      if (!user) {
+        res.status(404).send();
+      }
+      res.status(200).send({ user });
+    })
+    .catch(e => res.status(400).send());
 });
 
 app.listen(3000, () => {
