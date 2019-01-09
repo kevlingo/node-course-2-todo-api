1afdd50a2130635e133c46e81595aa7c71917b1f
FleetCooling <kevlingo@gmail.com>
2019-01-09 09:31:46 -0500

    Fixed GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -34,12 +34,12 @@ app.get('/todos/:id', (req, res) => {
     res.status(404).send();
   }
 
-  User.findById(id)
-    .then(user => {
-      if (!user) {
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
         res.status(404).send();
       }
-      res.status(200).send({ user });
+      res.status(200).send({ todo });
     })
     .catch(e => res.status(400).send());
 });
