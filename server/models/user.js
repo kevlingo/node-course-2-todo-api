f038856d9af7cf478f944ba7ddaf5fac5535e55d
FleetCooling <kevlingo@gmail.com>
2019-01-12 18:29:10 -0500

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -47,7 +47,7 @@ UserSchema.statics.findByToken = function(token) {
   let decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject();
   }
@@ -96,7 +96,7 @@ UserSchema.methods.generateAuthToken = function() {
         _id: user._id.toHexString(),
         access
       },
-      'abc123'
+      process.env.JWT_SECRET
     )
     .toString();
   user.tokens = user.tokens.concat([{ access, token }]);
