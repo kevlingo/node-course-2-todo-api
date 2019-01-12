d5810a3b5168875959f57bfe74f4011be32ad990
FleetCooling <kevlingo@gmail.com>
2019-01-12 14:29:00 -0500

    Added test for DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -269,3 +269,19 @@ describe('POST /users/login', () => {
     done();
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', done => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect(() => {
+        User.findById(users[0]._id)
+          .then(user => expect(user.tokens.length).toBe(0))
+          .catch(e => done(e));
+      })
+      .end();
+    done();
+  });
+});
